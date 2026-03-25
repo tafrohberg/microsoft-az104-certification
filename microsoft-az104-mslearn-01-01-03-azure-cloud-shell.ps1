4c5557027ec5a8728208651c0fdb92af3e11a29a
Trevor Frohberg <tafohberg@@@gmail...com>
2026-03-24 21:14:43 -0400

    Getting the file hearders prepped!

---
+++ microsoft-az104-mslearn-01-01-03-azure-cloud-shell.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    This is to help me learn more about Azure Cloud Shell!
+
+.DESCRIPTION
+    I want to earn my AZ-104 certification and this is pulling information 
+    currently from:
+
+    Microsoft Learn, AZ-104: Prerequisites for Azure administrators
+    Module 1, Unit 3, "How does Azure Cloud Shell work?"
+
+    My naming scheme is the "first" learning path for the AZ-104 in MS Learn.
+    Then module 1, unit 3. *shrug* it helps me order them.
+
+.EXAMPLE
+    How to download the Azure Cloud Shell module for PowerShell and some
+    other tips for the native Azure Cloud Shell from the Azure Portal.
+ 
+.NOTES
+    Author: Trevor Frohberg
+    Date: 2026-03-24
+    Version: 1.0
+
+.LINK
+    https://learn.microsoft.com/en-us/training/paths/az-104-administrator-prerequisites/
+    https://learn.microsoft.com/en-us/training/modules/intro-to-azure-cloud-shell/3-how-azure-cloud-shell-works
+#>
